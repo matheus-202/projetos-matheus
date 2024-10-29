ec8512710926eb2b792733d803c8fd8bb01247fa
Matheus da Silva Francisco <matheussilvafrancisco512@gmail.com>
2024-10-29 10:55:33 -0300

    oi

--- atividades.py
+++ atividades.py
@@ -1,11 +1,12 @@
-resposta = 1
-resposta = 2
-
 print ('Matheus está online')
-print(input('você deseja falar com ele?\ndigite 1 - para falar com ele  2- para ir dormir: '))
+resposta = input('você deseja falar com ele?\ndigite 1 - para falar com ele  2- para ir dormir: ')
 
-if resposta == 1: 
+if resposta == '1': 
     print ( input('mande um oi para ele: '))
-    print ('mande um oi para ele: ')
 
-    
+
+elif resposta == '2':
+    print('vá dormir')
+
+else:
+    print('Erro')
--- negativououpositivo.py
+++ negativououpositivo.py
@@ -2,8 +2,11 @@ print('Este programa identifica se seu número é negativo ou positivo')
 
 n= int(input('Insira o número desejado: '))
 
-if n >=0 and n<=0:
+if n >=0 :
         print('seu número é positivo')
 
-else:
+elif n <=0:
         print('seu número é negativo')
+
+else:
+        print('erro')
